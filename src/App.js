76f021b3d832cd78b1a7aad741befe2c85ecc696
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-04-29 18:48:21 +0300

    Add initial structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    loading: false,
+    username: ""
+  };
+
+  handleUsernameChange = e => {
+    this.setState({
+      username: e.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    this.setState({
+      loading: true
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <input
+            onChange={this.handleUsernameChange}
+            value={this.state.username}
+          />
+          <button onClick={this.handleSubmit}>Load</button>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.loading && <p className="App-intro">Loading</p>}
       </div>
     );
   }
