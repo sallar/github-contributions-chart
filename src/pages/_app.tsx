c31000542926926bccfaae1016d013ca8dd63cd4
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2019-11-10 14:43:05 +0300

    Add types for _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import 'normalize.css/normalize.css'
-import '../styles/index.css'
-import '../styles/App.css'
+import "normalize.css/normalize.css";
+import "../styles/index.css";
+import "../styles/App.css";
 
-import Head from 'next/head'
+import Head from "next/head";
+import { AppContext } from "next/app";
+import { ReactPropTypes } from "react";
 
-export default ({Component, pageProps}) => <>
-  <Head>
-    <title>GitHub Contributions Chart Generator</title>
-  </Head>
-  <Component {...pageProps} />
-</>
+export default ({ Component, pageProps }: AppContext & { pageProps: any }) => (
+  <>
+    <Head>
+      <title>GitHub Contributions Chart Generator</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
