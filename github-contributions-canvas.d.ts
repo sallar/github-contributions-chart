0506aadeb845932d99022c50c4bad421bec12f21
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2019-11-10 19:04:45 +0300

    Temporarily update d.ts file

--- github-contributions-canvas.d.ts
+++ github-contributions-canvas.d.ts
@@ -1,10 +1,32 @@
 declare module "github-contributions-canvas" {
-  type Props = {
-    data: string | null;
+  export type YearData = {
+    year: string;
+    total: number;
+    range: {
+      start: string;
+      end: string;
+    };
+  };
+
+  export type ContributionData = {
+    date: string;
+    count: number;
+    color: string;
+    intensity: number;
+  };
+
+  export type DrawContributionsProps = {
+    data: {
+      years: YearData[];
+      contributions: ContributionData[];
+    };
     username: string;
     themeName: string;
     footerText: string;
   };
 
-  function drawContributions(canvas: HTMLCanvasElement, props: Props);
+  function drawContributions(
+    canvas: HTMLCanvasElement,
+    props: DrawContributionsProps
+  ): void;
 }
