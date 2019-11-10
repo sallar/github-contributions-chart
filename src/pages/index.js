bea6aa122f5ad21a53189cf39261d1fa60cfd1b9
Sallar Kaboli <sallar@kaboli.org>
2019-11-10 23:11:17 +0200

    Upgrade app and use hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,127 +1,79 @@
-import React, { Component } from "react";
+import React, { Component, useRef, useState, useEffect } from "react";
 import { download, uploadToTwitter, fetchData } from "../utils/export";
 import ThemeSelector from "../components/themes";
 
-class App extends Component {
-  canvas = null;
-  inputRef = null;
+const App = () => {
+  const inputRef = useRef();
+  const canvasRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [theme, setTheme] = useState("standard");
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-  state = {
-    loading: false,
-    data: null,
-    error: null,
-    username: "",
-    theme: "standard"
-  };
-
-  componentDidMount() {
-    if (this.inputRef) {
-      this.inputRef.focus();
+  useEffect(() => {
+    if (!data) {
+      return;
     }
-  }
+    draw();
+  }, [data, theme]);
 
-  handleUsernameChange = e => {
-    this.setState({
-      username: e.target.value
-    });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.setState({ loading: true, error: null });
-    fetchData(this.state.username)
+
+    setLoading(true);
+    setError(null);
+    setData(null);
+
+    fetchData(username)
       .then(data => {
+        setLoading(false);
+
         if (data.years.length === 0) {
-          return this.setState({
-            error: "Could not find your profile",
-            data: null,
-            loading: false
-          });
+          setError("Could not find your profile");
+        } else {
+          setData(data);
+          inputRef.current.blur();
         }
-        this.setState({ data, loading: false }, () => {
-          this.draw();
-          this.inputRef.blur();
-        });
       })
       .catch(err => {
-        this.setState({
-          loading: false,
-          error: "I could not check your profile successfully..."
-        });
+        console.log(err);
+        setLoading(false);
+        setError("I could not check your profile successfully...");
       });
   };
 
-  handleChangeTheme = themeName => {
-    this.setState({ theme: themeName }, () => {
-      return this.canvas && this.draw();
-    });
+  const handleChangeTheme = themeName => {
+    setTheme(themeName);
   };
 
-  download = e => {
+  const onDownload = e => {
     e.preventDefault();
-    download(this.canvas);
+    download(canvasRef.current);
   };
 
-  onShareTwitter = e => {
+  const onShareTwitter = e => {
     e.preventDefault();
-    uploadToTwitter(this.canvas);
+    uploadToTwitter(canvasRef.current);
   };
 
-  async draw() {
-    if (!this.canvas) {
-      return this.setState({
-        error: "Something went wrong... Check back later."
-      });
+  const draw = async () => {
+    if (!canvasRef.current || !data) {
+      setError("Something went wrong... Check back later.");
+      return;
     }
+
     const { drawContributions } = await import("github-contributions-canvas");
-    drawContributions(this.canvas, {
-      data: this.state.data,
-      username: this.state.username,
-      themeName: this.state.theme,
+
+    drawContributions(canvasRef.current, {
+      data,
+      username: username,
+      themeName: theme,
       footerText: "Made by @sallar & friends - github-contributions.now.sh"
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="App-logo">
-            <img src="/tentocats.jpg" width={200} alt="Tentocats" />
-            <h1>GitHub Contributions Chart Generator</h1>
-            <h4>All your contributions in one image!</h4>
-          </div>
-          {this._renderForm()}
-          <ThemeSelector
-            currentTheme={this.state.theme}
-            onChangeTheme={this.handleChangeTheme}
-          />
-          {this._renderGithubButton()}
-          <footer>
-            <p>
-              Not affiliated with GitHub Inc. Octocat iullustration made by{" "}
-              <a
-                href="https://octodex.github.com/tentocat/"
-                rel="noopener nofollow"
-              >
-                GitHub design team
-              </a>
-              .
-            </p>
-          </footer>
-        </header>
-        <section className="App-content">
-          {this.state.loading && this._renderLoading()}
-          {this.state.data !== null &&
-            !this.state.loading &&
-            this._renderGraphs()}
-          {this.state.error !== null && this._renderError()}
-        </section>
-      </div>
-    );
-  }
-
-  _renderGithubButton = () => {
+  const _renderGithubButton = () => {
     return (
       <div className="App-github-button">
         <a
@@ -137,23 +89,28 @@ class App extends Component {
     );
   };
 
-  _renderLoading = () => {
+  const _renderLoading = () => {
     return (
-      <div className="App-loading">
-        <img src={"/loading.gif"} alt="Loading..." width={200} />
-        <p>Please wait, I{`'`}m visiting your profile...</p>
+      <div className="App-centered">
+        <div className="App-loading">
+          <img src={"/loading.gif"} alt="Loading..." width={200} />
+          <p>Please wait, I’m visiting your profile...</p>
+        </div>
       </div>
     );
   };
 
-  _renderGraphs = () => {
+  const _renderGraphs = () => {
     return (
-      <div className="App-result">
+      <div
+        className="App-result"
+        style={{ display: data !== null && !loading ? "block" : "none" }}
+      >
         <p>Your chart is ready!</p>
         <div className="App-buttons">
           <button
             className="App-download-button"
-            onClick={this.download}
+            onClick={onDownload}
             type="button"
           >
             Download the Image
@@ -161,31 +118,30 @@ class App extends Component {
           or
           <button
             className="App-twitter-button"
-            onClick={this.onShareTwitter}
+            onClick={onShareTwitter}
             type="button"
           >
             Share on Twitter
           </button>
         </div>
 
-        <canvas ref={el => (this.canvas = el)} />
+        <canvas ref={canvasRef} />
       </div>
     );
   };
 
-  _renderForm = () => {
+  const _renderForm = () => {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
-          ref={ref => {
-            this.inputRef = ref;
-          }}
+          ref={inputRef}
           placeholder="Your GitHub Username"
-          onChange={this.handleUsernameChange}
-          value={this.state.username}
+          onChange={e => setUsername(e.target.value)}
+          value={username}
           id="username"
+          autoFocus
         />
-        <button type="submit" disabled={this.state.username.length <= 0}>
+        <button type="submit" disabled={username.length <= 0}>
           <span role="img" aria-label="Stars">
             ✨
           </span>{" "}
@@ -195,13 +151,48 @@ class App extends Component {
     );
   };
 
-  _renderError = () => {
+  const _renderError = () => {
     return (
-      <div className="App-error">
-        <p>{this.state.error}</p>
+      <div className="App-error App-centered">
+        <p>{error}</p>
       </div>
     );
   };
-}
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-logo">
+          <img src="/tentocats.jpg" width={200} alt="Tentocats" />
+          <h1>GitHub Contributions Chart Generator</h1>
+          <h4>All your contributions in one image!</h4>
+        </div>
+        {_renderForm()}
+        <ThemeSelector
+          currentTheme={theme}
+          onChangeTheme={themeName => setTheme(themeName)}
+        />
+        {_renderGithubButton()}
+        <footer>
+          <p>
+            Not affiliated with GitHub Inc. Octocat iullustration made by{" "}
+            <a
+              href="https://octodex.github.com/tentocat/"
+              rel="noopener nofollow"
+            >
+              GitHub design team
+            </a>
+            .
+          </p>
+        </footer>
+      </header>
+      <section className="App-content">
+        {loading && _renderLoading()}
+        {error !== null && _renderError()}
+        {_renderGraphs()}
+      </section>
+    </div>
+  );
+};
 
 export default App;
